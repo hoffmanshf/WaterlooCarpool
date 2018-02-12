bb3818dad3393953b3e542c95f2f26197f45eba2
hoffman <hoffmanshf@outlook.com>
2018-02-12 11:14:11 -0500

    Frontend temp

---
+++ src/main/java/com/hoffman/carpool/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.hoffman.carpool.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+}
