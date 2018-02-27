57b9292a1d9355c8375ee0d175bbb72b3314d9e3
hoffman <hoffmanshf@outlook.com>
2018-02-27 14:23:53 -0500

    updated userfront

--- src/main/resources/static/js/calendar/demo.js
+++ src/main/resources/static/js/calendar/demo.js
@@ -4,39 +4,39 @@ $(document).ready( function(){
 	theMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 	theDays = ["S", "M", "T", "W", "T", "F", "S"];
-    events = [
-      [
-        "4/"+month+"/"+year, 
-        'Meet a friend', 
-        '#', 
-        '#177bbb', 
-        'Contents here'
-      ],
-      [
-        "7/"+month+"/"+year, 
-        'Kick off meeting!', 
-        '#', 
-        '#1bbacc', 
-        'Have a kick off meeting with .inc company'
-      ],
-      [
-        "17/"+month+"/"+year, 
-        'Milestone release', 
-        '#', 
-        '#fcc633', 
-        'Contents here'
-      ],
-      [
-        "19/"+month+"/"+year, 
-        'A link', 
-        'http://www.google.com', 
-        '#e33244'
-      ]
-    ];
+    // events = [
+    //   [
+    //     "4/"+month+"/"+year,
+    //     'Meet a friend',
+    //     '#',
+    //     '#177bbb',
+    //     'Contents here'
+    //   ],
+    //   [
+    //     "7/"+month+"/"+year,
+    //     'Kick off meeting!',
+    //     '#',
+    //     '#1bbacc',
+    //     'Have a kick off meeting with .inc company'
+    //   ],
+    //   [
+    //     "17/"+month+"/"+year,
+    //     'Milestone release',
+    //     '#',
+    //     '#fcc633',
+    //     'Contents here'
+    //   ],
+    //   [
+    //     "19/"+month+"/"+year,
+    //     'A link',
+    //     'http://www.google.com',
+    //     '#e33244'
+    //   ]
+    // ];
     $('#calendar').calendar({
         months: theMonths,
         days: theDays,
-        events: events,
+        // events: events,
         popover_options:{
             placement: 'top',
             html: true
