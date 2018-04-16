661589d050189a66d9357a7f4185d371cf1d7c6d
hoffman <hoffmanshf@outlook.com>
2018-04-16 10:34:02 -0400

    added empty constructor

--- src/main/java/com/hoffman/carpool/domain/constant/AccountBaseURL.java
+++ src/main/java/com/hoffman/carpool/domain/constant/AccountBaseURL.java
@@ -1,7 +1,6 @@
 package com.hoffman.carpool.domain.constant;
 
 public class AccountBaseURL {
-
     public static final String driverAccountBaseURL = "http://localhost:8080/account/driverAccount";
     public static final String driverAccountSearchBaseURL = "http://localhost:8080/account/driverAccount/search";
     public static final String driverAccountSearchPassengerBaseURL = "http://localhost:8080/account/driverAccount/searchPassenger";
@@ -9,4 +8,8 @@ public class AccountBaseURL {
     public static final String riderAccountBaseURL = "http://localhost:8080/account/riderAccount";
     public static final String riderAccountSearchBaseURL = "http://localhost:8080/account/riderAccount/search";
     public static final String riderAccountSearchPassengerBaseURL = "http://localhost:8080/account/riderAccount/searchPassenger";
+
+    public AccountBaseURL() {
+        // empty to avoid instantiating this constant class
+    }
 }
--- src/main/java/com/hoffman/carpool/domain/constant/AccountType.java
+++ src/main/java/com/hoffman/carpool/domain/constant/AccountType.java
@@ -3,4 +3,8 @@ package com.hoffman.carpool.domain.constant;
 public class AccountType {
     public static final String riderAccountType = "riderAccount";
     public static final String driverAccountType = "driverAccount";
+
+    public AccountType() {
+        // empty to avoid instantiating this constant class
+    }
 }
