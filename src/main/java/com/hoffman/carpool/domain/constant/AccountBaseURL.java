f0f3a286825b36f2ac658106cb1da027309c56ca
hoffman <hoffmanshf@outlook.com>
2018-04-17 21:53:21 -0400

    typo

--- src/main/java/com/hoffman/carpool/domain/constant/AccountBaseURL.java
+++ src/main/java/com/hoffman/carpool/domain/constant/AccountBaseURL.java
@@ -1,15 +1,15 @@
 package com.hoffman.carpool.domain.constant;
 
 public class AccountBaseURL {
-    public static final String driverAccountBaseURL = "https://waterloocarpool-demo.herokuapp.com/account/driverAccount";
-    public static final String driverAccountSearchBaseURL = "https://waterloocarpool-demo.herokuapp.com/account/driverAccount/search";
-    public static final String driverAccountSearchPassengerBaseURL = "https://waterloocarpool-demo.herokuapp.com/account/driverAccount/searchPassenger";
+    public static final String driverAccountBaseURL = "http://localhost:8080/account/driverAccount";
+    public static final String driverAccountSearchBaseURL = "http://localhost:8080/account/driverAccount/search";
+    public static final String driverAccountSearchPassengerBaseURL = "http://localhost:8080/account/driverAccount/searchPassenger";
 
-    public static final String riderAccountBaseURL = "https://waterloocarpool-demo.herokuapp.com/account/riderAccount";
-    public static final String riderAccountSearchBaseURL = "https://waterloocarpool-demo.herokuapp.com/account/riderAccount/search";
-    public static final String riderAccountSearchPassengerBaseURL = "https://waterloocarpool-demo.herokuapp.com/account/riderAccount/searchPassenger";
+    public static final String riderAccountBaseURL = "http://localhost:8080/account/riderAccount";
+    public static final String riderAccountSearchBaseURL = "http://localhost:8080/account/riderAccount/search";
+    public static final String riderAccountSearchPassengerBaseURL = "http://localhost:8080/account/riderAccount/searchPassenger";
   
-  public AccountBaseURL() {
+    public AccountBaseURL() {
         // empty to avoid instantiating this constant class
     }
 }
