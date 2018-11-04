164d61087568472ddfd573bb8a020d541c4d33df
hoffman <hoffmanshf@outlook.com>
2018-11-04 15:59:10 -0500

    fixed account url

--- src/main/java/com/hoffman/carpool/domain/constant/AccountBaseURL.java
+++ src/main/java/com/hoffman/carpool/domain/constant/AccountBaseURL.java
@@ -1,13 +1,13 @@
 package com.hoffman.carpool.domain.constant;
 
 public class AccountBaseURL {
-    public static final String driverAccountBaseURL = "https://waterloocarpool-demo.herokuapp.com/postings/driver-offer";
-    public static final String driverAccountSearchBaseURL = "https://waterloocarpool-demo.herokuapp.com/postings/driver-offer/search";
-    public static final String driverAccountSearchPassengerBaseURL = "https://waterloocarpool-demo.herokuapp.com/postings/driver-offer/search-passenger";
+    public static final String driverAccountBaseURL = "https://waterloocarpool-demo.herokuapp.com/account/driver-offer";
+    public static final String driverAccountSearchBaseURL = "https://waterloocarpool-demo.herokuapp.com/account/driver-offer/search";
+    public static final String driverAccountSearchPassengerBaseURL = "https://waterloocarpool-demo.herokuapp.com/account/driver-offer/search-passenger";
 
-    public static final String riderAccountBaseURL = "https://waterloocarpool-demo.herokuapp.com/postings/rider-request";
-    public static final String riderAccountSearchBaseURL = "https://waterloocarpool-demo.herokuapp.com/postings/rider-request/search";
-    public static final String riderAccountSearchPassengerBaseURL = "https://waterloocarpool-demo.herokuapp.com/postings/rider-request/search-passenger";
+    public static final String riderAccountBaseURL = "https://waterloocarpool-demo.herokuapp.com/account/rider-request";
+    public static final String riderAccountSearchBaseURL = "https://waterloocarpool-demo.herokuapp.com/account/rider-request/search";
+    public static final String riderAccountSearchPassengerBaseURL = "https://waterloocarpool-demo.herokuapp.com/account/rider-request/search-passenger";
   
     public AccountBaseURL() {
         // empty to avoid instantiating this constant class
